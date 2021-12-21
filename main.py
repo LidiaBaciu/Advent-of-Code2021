ba32ff93e4eb3949b91f738eb41190ed3a62639a
Lidia <lbaciu@c-s.ro>
2021-12-21 12:59:58 +0200

    Added new solutions

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
+import re
+from itertools import product
+import math
 
-from utils import read_file, parse_command
+from utils import read_file, parse_command, read_bingo_inputs, get_range, calculateFuel
+from utils import TankFishes, ParanthesisMatcher, OctopusGrid, Cave
+from utils import get_adjacents, count_groups
 
 
 def day1():
@@ -17,6 +22,7 @@ def day1():
     print(results(tuple(
         sum(window) for window in zip(measurements, measurements[1:], measurements[2:])
     )))
+   
     
 def day2():
     commands = tuple(map(parse_command, read_file('input.txt')))
@@ -49,4 +55,182 @@ def day2():
             
     print(horizontal * depth)
 
-day2()
+
+def day3():
+    lines = np.loadtxt("input.txt", "U")
+    bits = int(len(lines[0]))
+    data = lines.view('U1').astype(int).reshape(lines.shape[0], bits)
+    pow2 = 1 << np.arange(bits)[::-1]
+    
+    print(pow2[1:5])
+    
+    nb_ones = (data == 1).sum(axis=0)
+    nb_zeros = (data == 0).sum(axis=0)
+    gamma_rate = pow2.dot(nb_ones > nb_zeros)
+    epsilon_rate = pow2.dot(nb_ones < nb_zeros)
+    result = gamma_rate * epsilon_rate
+    
+    print(result)
+    
+    a = b = data
+    for i in range(bits):
+        a_col = a[:, i]
+        b_col = b[:, i]
+        a = a[a_col == (a_col.sum()*2 >= a_col.size)] if len(a) > 1 else a
+        b = b[b_col == (b_col.sum()*2 < b_col.size)] if len(b) > 1 else b
+        
+    result = pow2.dot(a[0]) * pow2.dot(b[0])
+    
+    print(result)
+    
+    
+def day4():
+    (drawn, boards) = read_bingo_inputs("input.txt")
+    masked_boards = [np.ma.masked_array(board) for board in boards]
+    
+    result = 0
+    for draw, board in product(drawn, masked_boards):
+        board.mask |= board.data == draw
+        line_sum = board.mask.sum(0)
+        column_sum = board.mask.sum(1)
+        if np.any(line_sum == 5) or np.any(column_sum == 5):
+            result = board.sum() * draw
+            break
+    print(result)
+    
+    winning_boards = set()
+    for draw, (index, board) in product(drawn, enumerate(masked_boards)):
+        board.mask |= board.data == draw
+        line_sum = board.mask.sum(0)
+        column_sum = board.mask.sum(1)
+        if np.any(line_sum == 5) or np.any(column_sum == 5):
+            if index not in winning_boards and len(winning_boards) == len(boards) - 1:
+                result = board.sum() * draw
+                break
+            winning_boards.add(index)
+    print(result)
+    
+    
+def day5():
+    lines = read_file("input.txt")
+    coordinates = np.array([re.match('(\d+),(\d+) -> (\d+),(\d+)', line).groups() for line in lines]).astype(int)
+    size = np.max(coordinates) + 1
+    
+    grid = np.zeros((size, size))
+    
+    x_same_indexes = coordinates[:, 0] == coordinates[:, 2]
+    y_same_indexes = coordinates[:, 1] == coordinates[:, 3]
+    hv_indexes = x_same_indexes | y_same_indexes
+    vh_lines = coordinates[hv_indexes]
+    
+    for x1, y1, x2, y2 in vh_lines:
+        y_range = get_range(y1, y2)
+        x_range = get_range(x1, x2)
+        grid[y_range, x_range] += 1
+        
+    result = (grid >= 2).sum()
+    print(result)
+    
+    grid = np.zeros((size, size))
+    for x1, y1, x2, y2 in coordinates:
+        y_range = get_range(y1, y2)
+        x_range = get_range(x1, x2)
+        grid[y_range, x_range] += 1
+        
+    result = (grid >= 2).sum()
+    print(result)
+    
+    
+def day6():
+    lines = np.loadtxt("input.txt", delimiter=",")
+    fish_tank = TankFishes(lines)
+    print(fish_tank.get_fishes(80))
+    # too slow
+    print(fish_tank.get_fishes(256))
+  
+    
+def day6_np():
+    days = 256
+    lines = np.loadtxt("input.txt", delimiter=",", dtype="uint32")
+    fish = np.zeros(9)
+    age, count = np.unique(lines, return_counts=True)
+    fish[age] = count
+    
+    gen = np.copy(fish)
+    for _ in range(days):
+        gen[7] += gen[0]
+        gen = np.roll(gen, -1)
+    result = sum(gen)
+    print(result)
+
+
+def day7():
+    lines = np.loadtxt("input.txt", delimiter=",", dtype="int32")
+    
+    median = round(np.median(lines))
+    result = sum([abs(median - nb) for nb in lines])
+    print(result)
+    
+    mean = np.mean(lines)
+    meanFloor = math.floor(mean)
+    meanCeil = math.ceil(mean)
+    
+    meanSubFloor = sum([calculateFuel(i) for i in abs(lines - meanFloor)])
+    meanSubCeil = sum([calculateFuel(i) for i in abs(lines - meanCeil)])
+    
+    result = min(meanSubCeil, meanSubFloor)
+    print(result)
+
+
+def day8():
+    pass
+
+
+def day9():
+    lines = read_file("input.txt")
+    data = np.array([list(map(int, [c for c in line])) for line in lines])
+    
+    len_rows = len(data)
+    len_cols = len(data[0])
+    rows = range(len_rows)
+    columns = range(len_cols)
+    
+    result = 0
+    for row in rows:
+        for column in columns:
+            if data[row][column] < min(get_adjacents(row, column, data)):
+                result += 1 + data[row][column]
+         
+    groups = []
+    for row in rows:
+        for column in columns:
+            groups.append(0)
+            count_groups(row, column, data, groups)
+        
+    gr = sorted(groups, reverse=True)[:3]
+    print(math.prod(gr))
+    
+    
+def day10():
+    lines = read_file("input.txt")
+    matcher = ParanthesisMatcher(lines)
+    print(matcher.find_score())
+    print(matcher.find_scores())
+    
+    
+def day11():
+    lines = np.loadtxt("input.txt", "U")
+    bits = int(len(lines[0]))
+    data = lines.view('U1').astype(int).reshape(lines.shape[0], bits)
+
+    octopuses = OctopusGrid(data)
+    # print(octopuses.count_flashes())
+    print(octopuses.get_step())
+                
+def day12():
+    lines = read_file("input.txt")
+    cave = Cave(lines)
+    print(cave.count('start', set(), False))
+    print(cave.count('start', set(), True))
+
+day12()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import numpy as np
 
 def read_file(file):
     with open(file, 'r', encoding='utf8') as f:
@@ -6,3 +8,273 @@ def read_file(file):
 def parse_command(line: str) -> tuple[str, int]:
     command, amount = line.split()
     return command, int(amount)
+
+def read_bingo_inputs(file):
+    with open(file, 'r', encoding='utf8') as f:
+        drawn = list(map(int, f.readline().split(",")))
+        boards = [np.mat(board.replace("\n", ";")) for board in f.read()[1:-1].split("\n\n")]
+        return (drawn, boards)
+    
+def get_range(start, stop):
+    if stop >= start:
+        # Make range inclusive
+        return range(start, stop+1)
+    # Make range run backwards properly
+    return range(start, stop-1, -1)
+        
+class TankFishes:
+    def __init__(self, internal_timers):
+        self.internal_timers = internal_timers
+            
+    def total_fish(self, timer, days):
+        if days <= 0:
+            return 1
+        elif timer == 0:
+            return self.total_fish(6, days-1) + self.total_fish(8, days-1)
+        else:
+            return self.total_fish(timer-1, days-1)
+        
+    def get_fishes(self, days):
+        sum_fishes = 0
+        for timer in self.internal_timers:
+            sum_fishes += self.total_fish(timer, days)
+        return sum_fishes
+    
+def calculateFuel(rrange):
+    return sum([i for i in range(1, rrange+1)])
+
+def get_cell_value(row, column, matrix):
+    if row < 0 or column < 0 or row >= len(matrix) or column >= len(matrix):
+        return 9
+    return matrix[row, column]
+
+def is_low_point(row, column, matrix):
+    value = get_cell_value(row, column, matrix)
+    
+    if value < get_cell_value(row-1, column, matrix) \
+                and value < get_cell_value(row, column-1, matrix) \
+                and value < get_cell_value(row, column+1, matrix) \
+                and value < get_cell_value(row+1, column, matrix):
+        return True
+    return False
+
+def get_adjacents(row, column, matrix):
+    adjacents = []
+    rows = len(matrix)
+    columns = len(matrix[0])
+    if row + 1 < rows:
+        adjacents.append(matrix[row+1][column])
+    if row - 1 >= 0:
+        adjacents.append(matrix[row-1][column])
+    if column + 1 < columns:
+        adjacents.append(matrix[row][column+1])
+    if column - 1 >= 0:
+        adjacents.append(matrix[row][column-1])
+    return adjacents
+                
+def count_groups(row, column, matrix, groups):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    if row < 0 or row >= rows or column < 0 or column >= columns \
+        or matrix[row][column] == 9 or matrix[row][column] == -1:
+            return
+    matrix[row][column] = -1
+    groups[len(groups)-1] += 1
+    count_groups(row+1, column, matrix, groups)
+    count_groups(row-1, column, matrix, groups)
+    count_groups(row, column+1, matrix, groups)
+    count_groups(row, column-1, matrix, groups)
+    
+  
+    
+class ParanthesisMatcher:
+    def __init__(self, lines):
+        self.lines = lines
+        self.opening = ["(", "[", "{", "<"]
+        self.closing = [")", "]", "}", ">"]
+        self.matches = { 
+            "(": ")",
+            "[": "]",
+            "{": "}",
+            "<": ">"
+        }
+        self.scores = {
+            ")": 3,
+            "]": 57,
+            "}": 1197,
+            ">": 25137
+        }
+        self.autocomplete_scores = {
+            ")": 1,
+            "]": 2,
+            "}": 3,
+            ">": 4
+        }
+        
+    def get_match(self, character):
+        return self.matches[character]
+        
+    def get_score(self, character):
+        return self.scores[character]
+    
+    def find_illegal_character(self, line):
+        stack = []
+        for character in line:
+            if character in self.opening:
+                stack.append(character)
+            elif len(stack):
+                prev = stack.pop()
+                if character != self.get_match(prev):
+                    return character
+        return None
+    
+    def get_stack(self, line):
+        stack = []
+        for character in line:
+            if character in self.opening:
+                stack.append(character)
+            elif len(stack):
+                prev = stack[-1]
+                if character == self.get_match(prev):
+                    stack.pop()
+        return stack
+    
+    def find_score(self):
+        score = 0
+        
+        for line in self.lines:
+            illegal_character = self.find_illegal_character(line)
+            if illegal_character:
+                score += self.get_score(illegal_character)
+        return score
+    
+    def find_scores(self):
+        scores = []
+        for line in self.lines:
+            score = 0
+            illegal_character = self.find_illegal_character(line)
+            if illegal_character:
+                continue
+            stack = self.get_stack(line)
+            stack.reverse()
+            
+            for char in stack:
+                match = self.get_match(char)
+                score = score * 5 + self.autocomplete_scores[match]
+            scores.append(score)
+            
+        scores.sort()
+        return scores[len(scores) //2]
+    
+class OctopusGrid:
+    def __init__(self, grid):
+        self.grid = grid
+        self.steps = 100
+    
+    def get_adjacents(self, row, column):
+        adjacents = []
+        rows = len(self.grid)
+        columns = len(self.grid[0])
+        
+        if row + 1 < rows:
+            adjacents.append((row+1, column))
+        if row - 1 >= 0:
+            adjacents.append((row-1, column))
+        if column + 1 < columns:
+            adjacents.append((row, column+1))
+        if column - 1 >= 0:
+            adjacents.append((row, column-1))
+        
+        if (row + 1, column) in adjacents and (row, column + 1) in adjacents:
+            adjacents.append((row + 1, column + 1))
+        if (row + 1, column) in adjacents and (row, column - 1) in adjacents:
+            adjacents.append((row+1, column-1))
+        if (row-1, column) in adjacents and (row, column-1) in adjacents:
+            adjacents.append((row-1, column-1))
+        if (row-1, column) in adjacents and (row, column+1) in adjacents:
+            adjacents.append((row-1, column+1))
+        
+        return adjacents
+    
+    def flash(self, row, column, flashes):
+        flashes.append((row, column))
+        
+        for (x, y) in self.get_adjacents(row, column):
+            if (x, y) not in flashes:
+                self.grid[x, y] += 1
+                
+                if self.grid[x, y] > 9:
+                    self.flash(x, y, flashes)
+    
+    def count_flashes(self):
+        count = 0
+        for _ in range(self.steps):
+            flashes = []
+            self.grid += 1
+            
+            for row, line in enumerate(self.grid):
+                for column, n in enumerate(line):
+                    if self.grid[row, column] > 9 and (row, column) not in flashes:
+                        self.flash(row, column, flashes)
+                        
+            for (x, y) in flashes:
+                self.grid[x, y] = 0
+                
+            count += len(flashes)
+        return count
+    
+    def get_step(self):
+        step = 0
+        
+        while True:
+            flashes = []
+            self.grid += 1
+            step += 1
+            
+            for row, line in enumerate(self.grid):
+                for column, n in enumerate(line):
+                    if self.grid[row, column] > 9 and (row, column) not in flashes:
+                        self.flash(row, column, flashes)
+                        
+            for (x, y) in flashes:
+                self.grid[x, y] = 0
+            
+            if len(flashes) == len(self.grid) * len(self.grid[0]):
+                break
+        
+        return step
+    
+class Cave:
+    def __init__(self, inputs):
+        self.lines = inputs
+        self.edges = self.get_edges()
+            
+
+    def get_edges(self):
+        edges = {}
+        for line in self.lines:
+            src, dest = line.split("-")
+            if not dest == 'start' and not src == 'end':
+                edges[src] = edges.get(src, []) + [dest]
+            if not src == 'start' and not dest == 'end':
+                edges[dest] = edges.get(dest, []) + [src]
+        return edges
+    
+    
+    def count(self, node, prior_visited, allow_repeat) -> int:
+        visited = prior_visited | {node}
+        if node == "end":
+            return 1
+        counter = 0
+        for next in self.edges[node]:
+            if next == "start":
+                continue
+            elif next[0].isupper():
+                counter += self.count(next, visited, allow_repeat)
+            elif next not in visited:
+                counter += self.count(next, visited, allow_repeat)
+            elif allow_repeat:
+                counter += self.count(next, visited, False)
+        return counter
+    
+    
